b8a7e0629c1d445f900d3e0c25bf24465e6ee980
dizzthxl <plokplokplok@163.com>
2017-06-05 12:31:02 +0800

    bug fix
    bug fix

--- seaweedfs-server/start.sh
+++ seaweedfs-server/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #启动weed
-/usr/webserver/go/seaweedfs/weed $*
+/opt/go/seaweedfs/weed $*
 
-#/usr/webserver/go/seaweedfs/weed master -ip 192.168.126.245
+#/opt/go/seaweedfs/weed master -ip 192.168.126.245
 
-#/usr/webserver/go/seaweedfs/weed volume -dir=/tmp/data -max=5 -mserver=192.168.126.245:9333 -ip 192.168.126.245 -port=9080
+#/opt/go/seaweedfs/weed volume -dir=/tmp/data -max=5 -mserver=192.168.126.245:9333 -ip 192.168.126.245 -port=9080
